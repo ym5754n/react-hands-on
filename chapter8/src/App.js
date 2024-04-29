0855e5d3d479b50586aeac3955141f5cffc37229
ym5754n <29919765+ym5754n@users.noreply.github.com>
2024-04-29 18:21:45 +0900

    chapter8-5

--- chapter8/src/App.js
+++ chapter8/src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import GitHubUser from './components/GitHubUser';
 import RepositoryReadme from './components/RepositoryReadme';
 import UserRepositories from './components/UserRepositories';
+import SearchForm from "./components/SearchFrom";
 
 function App() {
-  const [login, ] = useState("ym5754n");
+  const [login, setLogin] = useState("ym5754n");
   const [repo, ] = useState("react-hands-on");
   return (
     <div className="">
       <header className="App-header">
+        <SearchForm value={login} onSearch={setLogin} />
         <GitHubUser login={login} />
         <UserRepositories login={login} />
         <RepositoryReadme login={login} repo={repo} />
