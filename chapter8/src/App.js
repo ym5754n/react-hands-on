85080c8d31c7b67fe52ae4cbce11bc82d0b98914
ym5754n <29919765+ym5754n@users.noreply.github.com>
2024-04-30 22:42:50 +0900

    chapter8-6

--- chapter8/src/App.js
+++ chapter8/src/App.js
@@ -1,38 +1,86 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import './App.css';
-import GitHubUser from './components/GitHubUser';
-import RepositoryReadme from './components/RepositoryReadme';
-import UserRepositories from './components/UserRepositories';
+// import GitHubUser from './components/GitHubUser';
+// import RepositoryReadme from './components/RepositoryReadme';
+// import UserRepositories from './components/UserRepositories';
 import SearchForm from "./components/SearchFrom";
 
-function App() {
-  const [login, setLogin] = useState();
-  const [repo, setRepo] = useState();
+import { GraphQLClient } from "graphql-request";
 
-  const handleSearch = login => {
-    if (login) return setLogin(login);
-    setLogin("");
-    setRepo("");
-  };
+const query = `
+  query findRepos($login:String!) {
+    user(login:$login) {
+      login
+      name
+      avatar_url: avatarUrl
+      repositories(first:100) {
+        totalCount
+        nodes {
+          name
+        }
+      }
+    }
+  }  
+`;
 
-  if (!login) {
-    return (
-      <div>
-        <header className="App-header">
-          <SearchForm value={login} onSearch={handleSearch} />
-        </header>
-      </div>
-    );
+const client = new GraphQLClient(
+  "https://api.github.com/graphql",
+  {
+    headers: {
+      Authorization: `Bearer hogehoge`
+    }
   }
+);
+
+const UserDetails = data => {
+  return (
+    <div className="githubUser">
+      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+    </div>
+  );
+}
+
+const List = ({ data = [], renderItem, renderEmpty }) => {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item, i) => (
+        <li key={i}>{renderItem(item)}</li>
+      ))}
+    </ul>
+  );
+}
+
+function App() {
+  const [login, setLogin] = useState("ym5754n");
+  const [userData, setUserData] = useState();
+
+  useEffect(() => {
+    client
+      .request(query, { login })
+      .then(({ user }) => user)
+      .then(setUserData)
+      .catch(console.error);
+  }, [login]);
+
+  if (!userData) return <p>Loading...</p>;
 
   return (
     <div className="">
       <header className="App-header">
-        <SearchForm value={login} onSearch={handleSearch} />
-        {login && <GitHubUser login={login} />}
+        <SearchForm value={login} onSearch={setLogin} />
+        <UserDetails {...userData} />
+        <List data={userData.repositories.nodes} renderItem={repo => <span>{repo.name}</span>} />
+        {/* {login && <GitHubUser login={login} />}
         {login && <UserRepositories login={login} repo={repo} onSelect={setRepo} />}
-        {login && repo && <RepositoryReadme login={login} repo={repo} />}
+        {login && repo && <RepositoryReadme login={login} repo={repo} />} */}
       </header>
     </div>
   );
