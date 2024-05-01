87a6a26a9e712061f0ab3504a4eea643a5f38055
ym5754n <29919765+ym5754n@users.noreply.github.com>
2024-05-01 22:59:57 +0900

    chapter9-3

--- chapter9/src/App.js
+++ chapter9/src/App.js
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { useState, Suspense, lazy } from "react";
 import './App.css';
 import Agreement from './components/Agreement';
-import Main from './components/Main';
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import ErrorBoundary from "./components/ErrorBoundary";
+
+const Main = lazy(() => import("./components/Main"));
 
 function App() {
   const [agree, setAgree] = useState(false);
 
   if (!agree) return <Agreement onAgree={() => setAgree(true)} />;
 
-  return <Main />;
+  return (
+    <ErrorBoundary>
+      <Suspense fallback={<ClimbingBoxLoader />}>
+        <Main />
+      </Suspense>
+    </ErrorBoundary>
+  );
 }
 
 export default App;
